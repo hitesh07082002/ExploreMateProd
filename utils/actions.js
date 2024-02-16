a3e26ee06d56f214b947b26629aaf2081e6fe7f5
hitesh07082002 <hiteshsadhwani777@gmail.com>
2024-02-17 04:29:00 +0530

    bug fix

--- utils/actions.js
+++ utils/actions.js
@@ -2,14 +2,13 @@ import OpenAI from 'openai';
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
-  
 });
 
 export const generateChatResponse = async (message) => {
   const response = await openai.chat.completions.create({
     messages: [
       { role: 'system', content: 'you are a helpful assistant' },
-      { role: 'user', content: message }
+      { role: 'user', content: message },
     ],
     model: 'gpt-3.5-turbo',
     temperature: 0,
